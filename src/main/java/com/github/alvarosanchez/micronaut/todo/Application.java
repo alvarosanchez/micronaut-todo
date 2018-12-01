99e6b07f1b963d74871df5edaf5bfc5ba6446b22
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-12-01 10:50:29 +0100

    Initial commit

---
+++ src/main/java/com/github/alvarosanchez/micronaut/todo/Application.java
@@ -0,0 +1,10 @@
+package com.github.alvarosanchez.micronaut.todo;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
